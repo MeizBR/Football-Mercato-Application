5d8d95a970e3c5dc409a81e411ab956b4d92cbb4
Maiez Ben Romdhane <maiezb@kpit.com>
2026-03-12 15:03:31 +0100

    started using football apis

--- web_scraping/ai_agents/python-chatgpt.py
+++ web_scraping/ai_agents/python-chatgpt.py
@@ -1,5 +1,9 @@
 from openai import OpenAI
 
+OPENAI_API_KEY = ""
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
 prompt = "You are an expert football transfer intelligence assistant helping build a football mercato application. Search for the latest transfer news, rumours, and official announcements across football. Your mission includes: identify confirmed transfers, identify credible transfer rumours, identify important contract renewals, identify loan deals, identify advanced negotiations and verbal agreements and ignore unrelated football match news"
 
 response = client.responses.create(
---
+++ web_scraping/football_api/main.py
@@ -0,0 +1,48 @@
+import http.client
+import json
+
+conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+
+headers = {
+    "x-apisports-key": ""
+}
+
+params = {
+    "league": "119",
+    "season": "2024"
+}
+
+conn.request("GET", "/teams?league={league}&season={season}".format(**params), headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+data_json = json.loads(data.decode("utf-8"))
+
+teams = []
+
+for team in data_json["response"]:
+    t = {
+        "team": {
+            "id": team["team"]["id"],
+            "name": team["team"]["name"],
+            "code": team["team"]["code"],
+            "country": team["team"]["country"],
+            "logo": team["team"]["logo"],
+        },
+        "stadium": {
+            "id": team["venue"]["id"],
+            "name": team["venue"]["name"],
+            "address": team["venue"]["address"],
+            "city": team["venue"]["city"],
+            "capacity": team["venue"]["capacity"],
+            "image": team["venue"]["image"],
+        }
+    }
+
+    teams.append(t)
+
+with open("Super-Liga-Clubs-Football-API.json", "w", encoding="utf-8") as f:
+    json.dump(teams, f, indent=2)
+
+print(teams)
